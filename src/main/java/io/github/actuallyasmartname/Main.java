2ade227f6412ff08a3805fa87400df4a8545d093
actuallyasmartname <77011692+actuallyasmartname@users.noreply.github.com>
2021-04-20 17:27:12 -0400

    stuff

--- src/main/java/io/github/actuallyasmartname/Main.java
+++ src/main/java/io/github/actuallyasmartname/Main.java
@@ -12,9 +12,6 @@ public class Main implements ModInitializer {
 	public static final ItemGroup ITEM_GROUP = FabricItemGroupBuilder.build(new Identifier("status_armor", "status_armor"), () -> new ItemStack(items.REGEN_DYE));
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
         items.registeritems();
 		armorregister.registerArmor();
 		System.out.println("Hello Fabric world!");
